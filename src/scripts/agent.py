00367e8f9c1d8a3a600438aae12027465818e7ad
Gautam Raj <156491596+gautam-rl@users.noreply.github.com>
2024-04-26 14:52:59 -0700

    cp

---
+++ src/scripts/agent.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from langchain import hub
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_openai import OpenAI
