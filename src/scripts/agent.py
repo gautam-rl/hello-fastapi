48f12848a841aff076f4c99ddd86702f937c19fa
Gautam Raj <156491596+gautam-rl@users.noreply.github.com>
2024-04-26 16:18:11 -0700

    cp

--- src/scripts/agent.py
+++ src/scripts/agent.py
@@ -5,16 +5,80 @@ from langchain.agents import AgentExecutor, create_react_agent
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_openai import OpenAI
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from tempfile import TemporaryDirectory
+
+from langchain_community.agent_toolkits import FileManagementToolkit
+from langchain_community.tools import HumanInputRun
+from langchain_community.tools.file_management.list_dir import ListDirectoryTool
 
 load_dotenv()
 
+
+def get_input() -> str:
+    """
+    Get input from the user.
+    """
+    print("Insert your text. Enter 'q' or press Ctrl-D (or Ctrl-Z on Windows) to end.")
+    contents = []
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        if line == "q":
+            break
+        contents.append(line)
+    return "\n".join(contents)
+
+
 if __name__ == "__main__":
-    tools = [TavilySearchResults(max_results=1)]
+    # We'll make a temporary directory to avoid clutter
+    working_directory = TemporaryDirectory()
+
+    # If you don't provide a root_dir, operations will default to the current working directory
+    toolkit = FileManagementToolkit(root_dir="/Users/gautam/source/hello-fastapi")
+    file_tools = []
+    file_tools += toolkit.get_tools()
+
+    human_tool = HumanInputRun(input_func=get_input)
+
+    #tools = [TavilySearchResults(max_results=1)]
+    tools = file_tools + [human_tool]
 
     # Get the prompt to use - you can modify this!
-    prompt = hub.pull("hwchase17/react")
+    prompt = PromptTemplate.from_template("""
+        Answer the following questions as best you can. You have access to the following tools:
+
+        {tools}
+
+        Use the following format:
+
+        Question: the input question you must answer
+
+        Thought: you should always think about what to do
+
+        Action: the action to take, should be one of [{tool_names}]
+
+        Action Input: the input to the action
+
+        Observation: the result of the action
+
+        ... (this Thought/Action/Action Input/Observation can repeat N times)
+
+        Thought: I now know the final answer
+
+        Final Answer: the final answer to the original input question
+
+        Begin!
+
+        Question: {input}
+
+        Thought:{agent_scratchpad}
+            """)
 
     # Choose the LLM to use
+    #llm = OpenAI(model="gpt-3.5-turbo-instruct")
     llm = OpenAI()
 
     # Construct the ReAct agent
@@ -23,4 +87,6 @@ if __name__ == "__main__":
     # Create an agent executor by passing in the agent and tools
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-    agent_executor.invoke({"input": "what is LangChain?"}) 
+    #agent_executor.invoke({"input": "Describe the project in the current directory"})
+    #agent_executor.invoke({"input": "List the current directory"})
+    print([tool for tool in tools if isinstance(tool, ListDirectoryTool)][0].run("pwd"))
