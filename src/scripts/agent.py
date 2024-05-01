d51e0b0d47f0f9c819f7bb67ec3aecae81edfcfc
Gautam Raj <156491596+gautam-rl@users.noreply.github.com>
2024-05-01 11:49:23 -0700

    Co-authored-by: Evan Sandler <evandsandler@users.noreply.github.com>

--- src/scripts/agent.py
+++ src/scripts/agent.py
@@ -1,98 +1,89 @@
 #!/usr/bin/env python
 
-from langchain import hub
-from langchain.agents import AgentExecutor, create_react_agent, create_tool_calling_agent
-from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_openai import OpenAI, ChatOpenAI
-from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
+import warnings
 from tempfile import TemporaryDirectory
 
+from dotenv import load_dotenv
 from langchain_community.agent_toolkits import FileManagementToolkit
-from langchain_community.tools import HumanInputRun
-from langchain_community.tools.file_management.list_dir import ListDirectoryTool
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI, OpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+warnings.filterwarnings("ignore")
+from pprint import pprint
+
+from langchain_community.document_loaders.generic import GenericLoader
+from langchain_community.document_loaders.parsers import LanguageParser
+from langchain_text_splitters import Language
+
+import bs4
+from langchain import hub
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
+llm = ChatOpenAI(model="gpt-4-turbo")
 
-def get_input() -> str:
+
+def load_codebase():
     """
-    Get input from the user.
+    Loads the codebase and returns the vector store.
     """
-    print("Insert your text. Enter 'q' or press Ctrl-D (or Ctrl-Z on Windows) to end.")
-    contents = []
+    # TODO If we have an existing vector store file, just load it.
+
+    loader = GenericLoader.from_filesystem(
+        "/Users/gautam/source/runloop/java/src/main/java/ai/runloop/net/",
+        glob="**/*",
+        suffixes=[".java"],
+        show_progress=True,
+        parser=LanguageParser(language="java"),
+    )
+    docs = loader.load()
+
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    vector = FAISS.from_documents(docs, embeddings)
+    vector.save_local("runloop_java.vectorstore")
+    return vector
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+def chat_with_codebase(vector):
+    retriever = vector.as_retriever()
+    template = """
+    Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+
+    {context}
+
+    Question: {question}
+
+    Helpful Answer:"""
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+        | StrOutputParser()
+    )
+
     while True:
-        try:
-            line = input()
-        except EOFError:
-            break
-        if line == "q":
-            break
-        contents.append(line)
-    return "\n".join(contents)
+        question = input("Enter your question:")
+        print(f" Answer: {rag_chain.invoke(question)}")
 
 
 if __name__ == "__main__":
-    # We'll make a temporary directory to avoid clutter
-    working_directory = TemporaryDirectory()
-
-    # If you don't provide a root_dir, operations will default to the current working directory
-    toolkit = FileManagementToolkit(root_dir="/Users/gautam/source/hello-fastapi")
-    file_tools = []
-    file_tools += toolkit.get_tools()
-
-    #human_tool = HumanInputRun(input_func=get_input)
-
-    #tools = [TavilySearchResults(max_results=1)]
-    #tools = file_tools + [human_tool]
-    tools = file_tools
-
-    # Get the prompt to use - you can modify this!
-    prompt = ChatPromptTemplate.from_messages(
-        [("system",
-            """
-            Answer the following questions as best you can. You have access to the following tools:
-
-            {tools}
-
-            Use the following format:
-
-            Question: the input question you must answer
-
-            Thought: you should always think about what to do
-
-            Action: the action to take, should be one of [{tool_names}]
-
-            Action Input: the input to the action
-
-            Observation: the result of the action
-
-            ... (this Thought/Action/Action Input/Observation can repeat N times)
-
-            Thought: I now know the final answer
-
-            Final Answer: the final answer to the original input question
-
-            Begin!
-
-            Question: {input}
-
-            Thought:{agent_scratchpad}
-                """)
-                ])
-
-    # Choose the LLM to use
-    #llm = OpenAI(model="gpt-3.5-turbo-instruct")
-    #llm = OpenAI(model="gpt-4-turbo")
-    llm = ChatOpenAI(model="gpt-4-turbo")
-
-    # Construct the ReAct agent
-    agent = create_react_agent(llm, tools, prompt)
-    #agent = create_tool_calling_agent(llm, tools, prompt)
-
-    # Create an agent executor by passing in the agent and tools
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-
-    agent_executor.invoke({"input": "Describe the project in the current directory"})
-    #agent_executor.invoke({"input": "List the files in the current directory"})
-    #print([tool for tool in tools if isinstance(tool, ListDirectoryTool)][0].run("pwd"))
+    # If runloop_java.vectorstore exists, just load it.
+    if os.path.exists("runloop_java.vectorstore"):
+    else:
+        vector = load_codebase()
+    chat_with_codebase(vector)
